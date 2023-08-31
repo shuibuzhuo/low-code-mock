1c219ca7522a1c3d2657aa738ad8c4de64f57f2c
zhuotuo <zhuotuo@leagsoft.com>
2023-08-31 16:12:53 +0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const mockList = require('./mock/index')
+
+const app = new Koa()
+const router = new Router()
+
+async function getRes(fn, ctx) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      const res = fn(ctx)
+      resolve(res)
+    }, 1000)
+  })
+}
+
+mockList.forEach(item => {
+  const { method, url, response } = item
+
+  router[method](url, async (ctx) => {
+    const res = await getRes(response, ctx)
+    ctx.body = res
+  })
+})
+
+app.use(router.routes())
+
+app.listen(3001, () => {
+  console.log('app started at port 3001...')
+})
---
+++ mock/index.js
@@ -0,0 +1,7 @@
+const test = require('./test')
+
+const mockList = [
+  ...test
+]
+
+module.exports = mockList
